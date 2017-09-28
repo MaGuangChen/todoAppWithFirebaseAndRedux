e3dd45e97be97c4f6f7f5a67d80d3b5b764ac3fc
Paul Ma <kwn791122@gmail.com>
2017-09-28 18:56:27 +0800

    run webpack on heroku

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -150,7 +150,7 @@ describe('Actions', () => {
 
         expect(mockActions[0].type).toEqual('ADD_TODOS');
         expect(mockActions[0].todos.length).toEqual(1);
-        expect(mockActions[0].todos.text).toEqual('Something to do');
+        expect(mockActions[0].todos[0].text).toEqual('Something to do');
         // 這邊叫完done後才去跑afterEach喔
         done();
       }, done)
